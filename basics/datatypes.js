be8a4f1878a617e6fd337a787699e5dc22f2f472
AbhishekChamarajanagar <abhisheknayakaks2341@gmail.com>
2025-04-29 16:29:35 +0000

    edited

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,1 +1,26 @@
 // alert("hello")  we are using nodejs not browser
+let name ="Abhishek"
+let age = 30 
+let isloggedin = true
+console.log(typeof(undefined))   //output undefined
+console.log(typeof(null))   // output object
+
+
+/*
+
+datatypes 
+
+1)string
+2)integer
+3)boolean
+
+
+
+
+null -it is standalone value  meansd it is empty value and null is a object 
+undefined in the sense we have not assigned any value to that
+
+
+symbols -it is used for uniquiness 
+
+*/
